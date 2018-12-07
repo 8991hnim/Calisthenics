df471036337a0ace7ab07c9b03879560ff22d862
vinhnk <vinhnk@winds.vn>
2018-12-07 14:03:47 +0700

    check valid email

--- presenter/signupPresenter.js
+++ presenter/signupPresenter.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
 	$('#btnSignUp').click(function(){
 
 		var emailSignup = $('#txtEmailSignUp').val();
@@ -8,45 +7,50 @@ $(document).ready(function(){
 		var accountSignup = $('#txtAccountSignUp').val();
 		var passwordSignup = $('#txtPasswordSignUp').val();
 		var confirmPasswordSignup = $('#txtConfirmPasswordSignUp').val();
-		
+
+		var emailValid = new RegExp('^[A-z][A-z0-9_.]{5,32}[^.]@[a-z]{2,}(.[a-z]{2,5}){1,2}$');
 
 		if(emailSignup.length == 0 || userNameSignup.length == 0 || accountSignup.length == 0 || passwordSignup.length == 0 || confirmPasswordSignup.length == 0){
 			$('#signupAlert').html('<div class="alert alert-danger" style="color: red; font-weight: bolder;">Please fill in the blank. </div>');
             $(".bg-text").height(480);
-		}
-		else
-			if(passwordSignup != confirmPasswordSignup){
-				$('#signupAlert').html('<div class="alert alert-danger" style="color: red; font-weight: bolder;">The passwords you entered do not match.</div>');
-	            $(".bg-text").height(500);
-			}
-			else{
-				$.ajax({
+		} else 
+			if( !emailValid.test(emailSignup) ) { 
+				alert(emailSignup);
+				$('#signupAlert').html('<div class="alert alert-danger" style="color: red; font-weight: bolder;">The email invalid.</div>');
+	            $(".bg-text").height(480);
+			} else 
+				if(passwordSignup != confirmPasswordSignup){
+					$('#signupAlert').html('<div class="alert alert-danger" style="color: red; font-weight: bolder;">The passwords you entered do not match.</div>');
+		            $(".bg-text").height(500);
+				}
+				else{
+					$.ajax({
 
-					url: "model/signupLogic.php",
-					data: {
-						email: emailSignup,
-						userName: userNameSignup, 				
-						account: accountSignup, 
-						password: passwordSignup
-					},
-					type: "POST",
-					success: function(res){
-						
-						if (res=='existAccount'){
-							$('#signupAlert').html('<div class="alert alert-danger" style="color: red; font-weight: bolder; font-size: 15px;">Account already exists!</div>');
-				            $(".bg-text").height(480);
-						} 
-						else if(res=='success'){
-							alert('add success');
-							location.reload();
-						}
-					},
-					error: function(xhr, status, errorThrown) {
-		                console.log(errorThrown + status + xhr);
-		            }
-			
-				});
-			}
+						url: "model/signupLogic.php",
+						data: {
+							email: emailSignup,
+							userName: userNameSignup, 				
+							account: accountSignup, 
+							password: passwordSignup
+						},
+						type: "POST",
+						success: function(res){
+							
+							if (res=='existAccount'){
+								$('#signupAlert').html('<div class="alert alert-danger" style="color: red; font-weight: bolder; font-size: 15px;">Account already exists!</div>');
+					            $(".bg-text").height(480);
+							} 
+							else if(res=='success'){
+								alert('add success');
+								location.reload();
+							}
+						},
+						error: function(xhr, status, errorThrown) {
+			                console.log(errorThrown + status + xhr);
+			            }
+				
+					});
+				}
 
 	})
 
