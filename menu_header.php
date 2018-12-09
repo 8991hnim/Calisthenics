c3b657b8e1a341e17d6f05df6012530b07f07593
vinhnk <vinhnk@winds.vn>
2018-12-09 20:25:14 +0700

    UI profile

---
+++ menu_header.php
@@ -0,0 +1,76 @@
+<?php
+    include_once("config/core.php");
+    include_once("model/user.php");
+    if(isset($_GET["logout"])) {
+        session_destroy();
+        header("Location: {$home_url}index.php");
+    }else if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']){
+        $user = unserialize($_SESSION['current_user']);
+
+    } 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Calisthenics</title>
+    <script type="text/javascript" src="helper/jquery/jquery.js"></script>
+    <script type="text/javascript" src="helper/bootstrap/js/bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="helper/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="helper/hover/css/hover.css" media="all">
+    <link rel="stylesheet" type="text/css" href="helper/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="helper/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="helper/css/style.css">
+</head>
+
+<body>
+<div class="container-fluid">
+
+  <nav class="navbar navbar-expand-md navbar-dark menuHead fixed-top justify-content-end">
+  <a class= "nav-link" id="nameCompanyMenu" title="Home" href="index.php">MVTech</a>
+  
+    
+    <?php 
+
+        if(isset($user)){
+            if($user->type==1){
+                echo ' <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                              <a class="nav-link dropdown-toggle btnLoginHome" href="#" id="navbardrop" data-toggle="dropdown">
+                                '.$user->username.'
+                              </a>
+                              <div class="dropdown-menu dropdown-menu-right text-center">
+                                <a class="dropdown-item" href="#"><i class="fa fa-cogs"></i>&nbsp; Manage</a>
+                                <a class="dropdown-item" href="profile.php"><i class="fa fa-user"></i>&nbsp; Profile</a>
+                                <a class="dropdown-item" href="index.php?logout=true"><i class="fa fa-sign-out"></i>&nbsp; Log out</a>
+                              </div>
+                            </li>
+                        </ul>';
+            }else{
+                echo ' <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                              <a class="nav-link dropdown-toggle btnLoginHome" href="#" id="navbardrop" data-toggle="dropdown">
+                                '.$user->username.'
+                              </a>
+                              <div class="dropdown-menu dropdown-menu-right text-center">
+                                <a class="dropdown-item" href="#"><i class="fa fa-user"></i>&nbsp; Profile</a>
+                                <a class="dropdown-item" href="index.php?logout=true"><i class="fa fa-sign-out"></i>&nbsp; Log out</a>
+                              </div>
+                            </li>
+                        </ul>';
+            }
+        }
+        else{
+            echo '<li class="nav-item">
+                      <a class="nav-link btnLoginHome hvr-rotate" href="login.php">Log in</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link btnSignUpHome hvr-rotate" href="signup.php">Sign up</a>
+                  </li>
+                  ';
+        }
+     ?>
+                            
+</nav>
---
+++ profile.php
@@ -0,0 +1,85 @@
+<?php 
+	require"menu_header.php"
+ ?>
+
+<div class="container">
+	<div class="row mt-6"></div>
+	<p class="text-center text-light profile-thanks">MVTech thank Vinh!</p>
+	<p class="text-info pt-5">Account management</p>
+	<table class="table table-dark table-hover">
+    	<tbody>
+      		<tr>
+    			<td>Name</td>
+        		<td>Nguyen Khac Vinh</td>
+        		<td class="text-right edit-account"><a href="" class="text-warning" title="Edit"><i class="fa fa-edit"></i></a></td>
+      		</tr>
+      		<tr>
+        		<td>Email</td>
+        		<td>vinnk@gmail.com</td>
+        		<td class="text-right edit-account"><a href="" class="text-warning" title="Edit"><i class="fa fa-edit"></i></a></td>
+      		</tr>
+      		<tr>
+        		<td>Account</td>
+        		<td>vinhnk</td>
+        		<td class="text-right edit-account"><a href="" class="text-warning" title="Edit"><i class="fa fa-edit"></i></a></td>
+      		</tr>
+      		<tr>
+        		<td>Password</td>
+        		<td>For security reasons, we can not show your password.</td>
+        		<td class="text-right edit-account"><a href="" class="text-warning" title="Edit"><i class="fa fa-edit"></i></a></td>
+      		</tr>
+    	</tbody>
+  	</table>
+  	<p class="text-info mt-5 pt-5">Training results</p>
+  	<div class="row">
+  		<div class="col-12 col-sm-12 col-md-4 offset-md-0">
+  			<table class="table table-dark table-hover">
+		    	<tbody class="text-center">
+		      		<tr>
+		    			<td>Level 1</td>
+		      		</tr>
+		      		<tr>
+		        		<td>Day number: 12</td>
+		      		</tr>
+		      		<tr>
+		        		<td>Day trained: 6</td>
+		      		</tr>
+		    	</tbody>
+  			</table>
+  		</div>
+  		<div class="col-12 col-sm-12 col-md-4 offset-md-0">
+  			<table class="table table-dark table-hover">
+		    	<tbody class="text-center">
+		      		<tr>
+		    			<td>Level 1</td>
+		      		</tr>
+		      		<tr>
+		        		<td>Day number: 12</td>
+		      		</tr>
+		      		<tr>
+		        		<td>Day trained: 6</td>
+		      		</tr>
+		    	</tbody>
+  			</table>
+  		</div>
+  		<div class="col-12 col-sm-12 col-md-4 offset-md-0">
+  			<table class="table table-dark table-hover">
+		    	<tbody class="text-center">
+		      		<tr>
+		    			<td>Level 1</td>
+		      		</tr>
+		      		<tr>
+		        		<td>Day number: 12</td>
+		      		</tr>
+		      		<tr>
+		        		<td>Day trained: 6</td>
+		      		</tr>
+		    	</tbody>
+  			</table>
+  		</div>
+  	</div>
+</div>
+
+ <?php 
+	require"footer.php"
+ ?>
