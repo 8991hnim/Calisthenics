c42399ebac246e1e56c7f52748eb55aa2d44f23e
minh <8991hnim>
2018-12-21 13:44:41 +0700

    .

--- Sources/admin/manage_post.php
+++ Sources/admin/manage_post.php
@@ -3,50 +3,34 @@
     require"layout_header.php";
 
 ?>
-
-
-    <div class="content-page">
-	
-		<!-- Start content -->
-        <div class="content">
-            
-			<div class="container-fluid">
-
-															
-										
-						<div class="row">
-								<div class="col-xl-12">
-										<div class="breadcrumb-holder">
-												<h1 class="main-title float-left">POST</h1>
-												<ul class="breadcrumb float-right">
-													<li class="">
-														<a href="" class="btn btn-info">Create post</a>
-													</li>
-													<li class="">
-														
-													</li>
-												</ul>
-												<div class="clearfix"></div>
-										</div>
-								</div>
-						</div>
-						<!-- end row -->
-
+<div class="content-page">
+    <!-- Start content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="breadcrumb-holder">
+                        <h1 class="main-title float-left">POST</h1>
+                        <ul class="breadcrumb float-right">
+                            <li class="">
+                                <a href="" class="btn btn-info">Create post</a>
+                            </li>
+                            <li class="">
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
             </div>
-			<!-- END container-fluid -->
-
-		</div>
-		<!-- END content -->
-
+            <!-- end row -->
+        </div>
+        <!-- END container-fluid -->
     </div>
-	<!-- END content-page -->
-    
+    <!-- END content -->
+</div>
+<!-- END content-page -->
 <?php
     
     require"layout_footer.php";
 
 ?>
-
-
-
-
