1f69622059c8544ce1af274061942c981d3d6877
minh <8991hnim>
2018-12-29 21:08:21 +0700

    done manage level ^^ liverpull vo dich

--- Sources/js/addExercisePresenter.js
+++ Sources/js/addExercisePresenter.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     $('#title').text(" Add exercise to level " + level);
     var day = 1;
     var exerciseId = 1;
+    var arrayId = [];
+    var isExist = false;
 
     $('#selectDay').on('change', function() {
         day = this.value;
@@ -15,16 +17,51 @@ $(document).ready(function() {
     $('#btnSave').click(function() {
 
         // alert();
-        if(isEmpty()){
+        if (isEmpty()) {
             alert("Please fill in the blank.");
-        }else if (!isNumeric($('#setInp').val())) {
+        } else if (!isNumeric($('#setInp').val())) {
             alert("Set must be a number");
         } else if (!isNumeric($('#repInp').val())) {
             alert("Rep must be a number");
         } else if (!isNumeric($('#breakTimeInp').val())) {
             alert("Break time must be a number");
+        } else {
+            //lay ds exercise, kiem tra trung
+            $.ajax({
+                url: "../controller/exercise/GetExerciseByDay.php",
+                data: {
+                    level: level,
+                    day: day
+                },
+                type: "GET",
+                success: function(res) {
+                    arrayExercise = $.parseJSON(res);
+                    $.each(arrayExercise, function(index, value) {
+                        arrayId.push(value.id);
+                    })
+
+                    $.each(arrayId, function(index, value) {
+                        if (exerciseId == value) {
+                            isExist = true;
+                        }
+                    })
+
+                    if (isExist == true) {
+                        alert("This exercise already exist in day " + day);
+                        isExist = false;
+                    } else {
+                        addExerciseToLevel();
+                    }
+
+                },
+                error: function(xhr, status, errorThrown) {
+                    console.log(errorThrown + status + xhr);
+                }
+
+            });
         }
     })
+
 
     //add exercise vào select
     $.ajax({
@@ -45,14 +82,54 @@ $(document).ready(function() {
     });
 
     function isNumeric(s) {
-        var re = new RegExp("^[0-9]+");
+        var re = new RegExp("^[0-9]+$");
         return re.test(s);
     }
 
-    function isEmpty(){
-        if($('#setInp').val().trim() < 1 || $('#repInp').val().trim() < 1 || $('#breakTimeInp').val().trim() < 1){
+    function isEmpty() {
+        if ($('#setInp').val().trim() < 1 || $('#repInp').val().trim() < 1 || $('#breakTimeInp').val().trim() < 1) {
             return true;
         }
         return false;
     }
+
+    function addExerciseToLevel() {
+        alert(exerciseId);
+        $.ajax({
+            url: "../controller/exercise/AddExerciseLevel.php",
+            data: {
+                level: level,
+                day: day,
+                set: $('#setInp').val(),
+                rep: $('#repInp').val(),
+                exerciseId: exerciseId,
+                breakTime: $('#breakTimeInp').val()
+            },
+            type: "POST",
+            success: function(res) {
+                if (res != "success") {
+                    swal({
+                        title: "Opps :(",
+                        text: "Add failed!",
+                        icon: "warning"
+                    });
+                } else {
+                    swal({
+                            title: "Success",
+                            text: "Added!",
+                            icon: "success"
+                        })
+                        .then((value) => {
+                            var url = new URL(window.location.replace("http://localhost:8080/Calisthenics/Sources/admin/manage_level.php?level=" +level));
+                            window.location.href = url.href;
+                        });
+                }
+            },
+            error: function(xhr, status, errorThrown) {
+                console.log(errorThrown + status + xhr);
+            }
+
+        });
+    }
+
 })
