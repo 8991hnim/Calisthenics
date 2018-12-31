4ff8e76d476a372c833fddd88933212887a193e8
vinhnk <vinhnk@winds.vn>
2018-12-31 23:29:24 +0700

    linh tinh

--- Sources/admin/layout_header.php
+++ Sources/admin/layout_header.php
@@ -68,7 +68,7 @@
 						
                         <li class="list-inline-item dropdown notif">
                             <a class="nav-link dropdown-toggle nav-user" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                                <img src="assets/images/avatars/admin.png" alt="Profile image" class="avatar-rounded">
+                                <img src="assets/images/avatars/minh.jpg" alt="Profile image" class="avatar-rounded">
                             </a>
                             <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
                                 <!-- item-->
